d3eb4a1dee1dd132e2cde46c8e0606ba0ff7316a
ZloyIndy <ZzloyIndy@yandex.ru>
2022-02-20 23:41:03 +0300

    work with more than 2 numbers. Cleans code

--- calculator.rb
+++ calculator.rb
@@ -17,55 +17,94 @@ end
 $errors = []
 $operations = ['*','/','+','-','^']
 
-def validation(a,b,o)
-	validated = true
-	
-	if a.is_a?(Numeric) == false 
-		$errors << "Firsh character must be numeric"
+def calculate(problem)
+	problem = problem.split(/(\d+\.?\d*)/).reject(&:empty?)
+	if validationAll(problem)
+		j = 1
+		operationsOrder = [
+		['^'],
+		['/','*'],
+		['+','-']
+		]
+		operationsOrder.each_with_index{ |val,index|
+			val.each{ |v|
+				while problem.index(v)
+					#puts "#{v} is in #{problem} = #{problem.index(v)}"
+					puts "#{j})#{problem.join('')} = "
+					tempIndex = problem.index(v)
+					a = Float(problem[tempIndex-1],exception: false)
+					b = Float(problem[tempIndex+1],exception: false)
+					o = problem[tempIndex]
+					
+					problem.delete_at(tempIndex-1)
+					problem.delete_at(tempIndex-1)
+					problem.delete_at(tempIndex-1)
+					
+					problem.insert(tempIndex-1,compute(a,b,o).to_s)
+					j += 1
+				end
+			}
+		}
+		result = problem[-1]
+		return result
+	else
+		puts $errors
+		return false
 	end
-	if $operations.include?(o) == false
-		$errors << "Second character must be one of the following operations: #{$operations}"
-	end
-	if b.is_a?(Numeric) == false
-		$errors << "Third character must be numeric"
-	end
-	if o == '/' && b == 0
-		$errors << "You can't divide by 0 :(\r\n"
-	end
-	if $errors.length > 0
-		validated = false
-		$errors.unshift "\r\n__//List of errors//__"
-		$errors << 		"______________________"
-	end
-	return validated
+end
+
+def underline 
+	puts "______________________"
+end
+
+def validationAll (problem)
+	problem.each_cons(3) { |a,o,b|
+		a = Float(a,exception: false)
+		b = Float(b,exception: false)
+		if a.is_a?(Numeric) == false 
+		$errors << "Found not numeric character #{a} where numeric was expected"
+		end
+		if $operations.include?(o) == false
+			$errors << "Found exceptional operation #{o} where operation was expected. List of available operations: #{$operations}"
+		end
+		if b.is_a?(Numeric) == false
+			$errors << "Found not numeric character #{b} where numeric was expected"
+		end
+		if o == '/' && b == 0
+			$errors << "You can't divide by 0 :("
+		end
+		if $errors.length > 0
+			$errors.unshift "\r\n__//List of errors//__"
+			$errors << 		"______________________"
+		end
+		return true
+	}
 end
 
 def startMessage
 	$errors.clear
-	puts 'psst want some calculations? [y/n]'
-	#puts '[y/n] (empty string counts as Yes)'
-	getStart = gets.chomp.downcase
+	puts 'psst wanna calculate some? [y/n]'
 	
-	if getStart == 'y' || getStart == ''
-		puts "Please type in math problem with [2] numbers using one of the operations #{$operations} (like 2+2)"
+	getStart = gets.chomp.downcase
+	startAcceptable = ['y','','н']
+	if startAcceptable.include?(getStart)
+		puts "Please type in math problem using listed operations #{$operations} (like 2+2*3^2/1-9)"
+		puts "Brackets are not yet supported"
+		underline
 		getProblem = gets.chomp
-		
+		problem = getProblem.gsub(' ', '')
 		#/(\d+\.?\d*)/
-		problem = getProblem.gsub(' ', '').split(/(\d+\.?\d*)/).reject(&:empty?)
-		if problem.length != 3
-			$errors << "The problem must contain 3 characters (like 2+2)"
-		end
-		a = Float(problem[0],exception: false)
-		b = Float(problem[2],exception: false)
-		o = problem[1]
-		if validation(a,b,o) 
-			puts "\r\n#{getProblem} = #{compute(a,b,o)}"
-			puts ''
-			startMessage
-		else 
+		
+		result = calculate(problem)
+		if result == false 
 			puts $errors
 			startMessage
-		end
+		else
+			#puts "\r\n#{getProblem} = #{result}"
+			puts "= #{result}"
+			underline
+			startMessage
+		end		
 	elsif getStart == 'n'
 		puts 'Bye!'
 	else 
